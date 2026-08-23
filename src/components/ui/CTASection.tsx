39de8d26f420230c9e9168820746b78b9583aa3c
JameMy0001 <jamerrmool@gmail.com>
2026-08-23 15:04:42 +0700

    feat: update Android APK download link

--- src/components/ui/CTASection.tsx
+++ src/components/ui/CTASection.tsx
@@ -16,7 +16,7 @@ export function CTASection() {
     }
   }, [])
 
-  const APK_URL = 'https://drive.google.com/file/d/1z0clSsGNCBE8f9T7MEvF8SvpycJzRvOM/view?usp=sharing'
+  const APK_URL = 'https://drive.google.com/file/d/1t9QoarVYsCo_nM3EoB7Fa6xiK2nKT9FR/view?usp=sharing'
 
   const handleAndroidDownload = () => {
     setDownloadStarted(true)
